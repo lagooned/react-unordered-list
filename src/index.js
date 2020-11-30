e4d62a995482134e3506e2075d895b56dc593fcb
Jared Engler <jared.lite@gmail.com>
2020-11-30 00:19:20 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <ul>
+    <li>listitemone</li>
+    <li>listitemtwo</li>
+    <li>listitemthree</li>
+  </ul>,
+  document.getElementById("root"))
